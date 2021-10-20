f76ab15445e0d346c23f20f1e196c126048a9e4c
MH-Movies <88358778+MH-Movies@users.noreply.github.com>
2021-10-20 12:08:04 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,13 +10,13 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+*Creator number : wa.me/917306670148*
 
 *Bot setting video : https://youtu.be/_D4ZYuUSXjs*
 
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
+*TG Group  :    https://t.me/movies_Hubh2*
 
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*TG Channel :   https://t.me/joinchat/uEyMpZ_OvSE2ZjU1*
 
 *Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
 
